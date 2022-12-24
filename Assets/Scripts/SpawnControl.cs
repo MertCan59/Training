2806924790d92118f9dcbb689c7190df23fdecaf
MertCan59 <mertcankoymen@gmail.com>
2022-12-24 16:23:47 +0300

    My All Codes

--- Assets/Scripts/SpawnControl.cs
+++ Assets/Scripts/SpawnControl.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SpawnControl : MonoBehaviour
 {
-    public GameObject enemy;
+    public GameObject [] enemy;
+
   void Start()
     {
-        InvokeRepeating("Spawn", 1.5f, 1.5f);
+        InvokeRepeating("Spawn", 3f, 3f);
     }
     void Spawn()
     {
         Vector3 randomSpawnLocation = new Vector3(Random.Range(-17f, 30f), 1f, Random.Range(-30f, 17f));
-        Instantiate(enemy, randomSpawnLocation, Quaternion.identity);
+        for(int i = 0; i < enemy.Length; i++)
+        {
+            Instantiate(enemy[i], randomSpawnLocation, Quaternion.identity);
+        }
     }
 
 }
